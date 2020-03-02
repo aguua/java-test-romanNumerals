238e72a0b54228124ea3c778b869fca4cdd12498
Agnieszka Harłozińska <AHarlozinska@advaoptical.com>
2020-03-02 16:56:04 +0100

    run first test

---
+++ src/RomanNumeral.java
@@ -0,0 +1,9 @@
+public class RomanNumeral {
+    int number;
+    public RomanNumeral(int num){
+        this.number = num;
+    }
+    public String getRomanNumeral(){
+        return "I";
+    }
+}
