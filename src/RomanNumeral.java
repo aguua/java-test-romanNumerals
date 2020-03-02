fef3c96e45c19711d54b0dc1173db1f5b1ed72ed
Agnieszka Harłozińska <AHarlozinska@advaoptical.com>
2020-03-02 19:58:29 +0100

    simple tests

--- src/RomanNumeral.java
+++ src/RomanNumeral.java
@@ -4,6 +4,22 @@ public class RomanNumeral {
         this.number = num;
     }
     public String getRomanNumeral(){
-        return "I";
+        switch (number) {
+            case 1:
+                return "I";
+            case 2:
+                return "II";
+            case 3:
+                return  "III";
+            case 5:
+                return "V";
+            case 6:
+                return "VI";
+            case 9:
+                return "IX";
+
+
+        }
+        return "";
     }
 }
